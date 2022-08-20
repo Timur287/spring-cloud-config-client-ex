38be980c74e8263be6df5db68e026127fae7d9f5
Timur287 <tmuxametov@gmail.com>
2022-08-20 15:33:49 +0300

    / commit

--- src/main/java/com/example/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ src/main/java/com/example/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -3,10 +3,8 @@ package com.example.springcloudconfigclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class SpringCloudConfigClientApplication {
 
     public static void main(String[] args) {
